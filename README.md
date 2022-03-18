2e17ee03b43eb4883146bf9fd5e8743fafadb40c
Darren Hall <dhall.public@gmail.com>
2022-03-18 15:43:17 -0400

    Fix integration tests to run independently of each other

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # A Simple Node REST Service
 
 ## Code structure
+
 This code is organized following conventional JavaScript standards.
 
 The project root directory contains project configuration files used by Git and NPM (such as .gitignore and package.json), as well as this README.md file which contains project information.
@@ -12,27 +13,61 @@ The project root directory contains project configuration files used by Git and 
 - ....**/src/data** contains objects and functions related to the persistence layer.
 - ....**/src/routes** defines the RESTful API endpoints, mapping URL paths to controller operations.
 - ....**/src/service** contains objects and functions related to executing the business operations.
-- **__test__** contains the application unit test suite and integration test suites.
+- \***\*test\*\*** contains the application unit test suite and integration test suites.
 
 Unit test source code files are named using the Jest file naming convention of `{object filename}.test.js`.
 Integration test source code files are names using the naming convention of `{object filename}.integration.test.js`.
 
 ## Intalling the code
+
 Clone this repository using the location specified by Git.
 
 Before running the code for the first time, update the application dependencies by executing the command below from the command line at the applucation root.
->   npm install
+
+> npm install
 
 ## Testing the code
+
 From a terminal window at the application root, execute the command below to run the unit test suite.
->   npm test
+
+> npm test
 
 From a terminal window at the application root, execute the command below to run the integration test suite.
->   npm run test:integration
+
+> npm run test:integration
 
 ## Running the code
+
 From a terminal window at the application root, execute the command below.
->   npm start
+
+> npm start
 
 To run the application in developer mode, execute the command below from a terminal window at the application root.
->   npm run start:dev
+
+> npm run start:dev
+
+## Service API
+
+- Get all songs
+  GET http://localhost:8000/songs/
+
+- Get song by ID
+  GET http://localhost:8000/songs/:id
+
+- Add a new song
+  POST http://localhost:8000/songs/
+
+- Update an existing song by ID
+  PUT http://localhost:8000/songs/:1d
+
+- Delete a song by ID
+  DELETE http://localhost:8000/songs/1
+
+## Message Structure
+
+[{
+"id": 0,
+"title": "{title}",
+"artist": "{artist}",
+"url": "{YouTube url}"
+}]
--- __tests__/integration/song.api.integration.test.js
+++ __tests__/integration/song.api.integration.test.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const request = require("supertest");
+const fs = require('fs');
 const loadSongRoutes = require("../../src/routes/song.routes");
+const instance = require("../../src/data/datastore");
 
 // configure and create runtime environment context
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 loadSongRoutes(app);
+
+beforeEach(() => {
+    try {
+        const file_descriptor = fs.openSync("./__data__/db.json");
+        const songs = JSON.parse(fs.readFileSync(file_descriptor, 'utf8'));
+        songs.forEach(song => instance.set(String(song.id), song));
+        fs.closeSync(file_descriptor);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+afterEach(() => {
+    instance.clear();
+})
+
 
 // Integrtion tests for the song API endpoints
 describe("Integration tests for song API endpoints", () => {
@@ -16,15 +34,15 @@ describe("Integration tests for song API endpoints", () => {
         // arange and act
         const url = "/songs";
         const input = {
-            "title": "La Mer (Beyond the Sea)",
-            "artist": "Bobby Darin",
-            "url": "https://www.youtube.com/watch?v=m8OlDPqYBLw"
+            "title": "I Only Have Eyes for You",
+            "artist": "The Flamingos",
+            "url": "https://www.youtube.com/watch?v=1_urvud-Oi0"
         };
         const result = {
-            "id": 1,
-            "title": "La Mer (Beyond the Sea)",
-            "artist": "Bobby Darin",
-            "url": "https://www.youtube.com/watch?v=m8OlDPqYBLw"
+            "id": (instance.size + 1),
+            "title": "I Only Have Eyes for You",
+            "artist": "The Flamingos",
+            "url": "https://www.youtube.com/watch?v=1_urvud-Oi0"
         };
     
         let response = await request(app).post(url).send(input);
@@ -36,12 +54,7 @@ describe("Integration tests for song API endpoints", () => {
     test ("GET /songs - success", async () => {
         // arange and act
         const url = "/songs";
-        const result = [{
-            "id": 1,
-            "title": "La Mer (Beyond the Sea)",
-            "artist": "Bobby Darin",
-            "url": "https://www.youtube.com/watch?v=m8OlDPqYBLw"
-        }];
+        const result = JSON.parse(fs.readFileSync('./__data__/db.json', 'utf8'));
     
         let response = await request(app).get(url);
 
@@ -51,12 +64,12 @@ describe("Integration tests for song API endpoints", () => {
 
     test ("PUT /songs/:id - success", async () => {
         // arange and act
-        const url = "/songs/1";
+        const url = "/songs/2";
         const input = {
-            "id": 1,
-            "title": "Rave On!",
-            "artist": "Buddy Holly",
-            "url": "https://www.youtube.com/watch?v=0IUV-QxwlRM"
+            "id": 2,
+            "title": "I Only Have Eyes for You",
+            "artist": "The Flamingos",
+            "url": "https://www.youtube.com/watch?v=1_urvud-Oi0"
         };
     
         let response = await request(app).put(url).send(input);
@@ -66,13 +79,14 @@ describe("Integration tests for song API endpoints", () => {
     })
 
     test ("GET /songs/:id - success", async () => {
+        console.log(instance);
         // arange and act
         const url = "/songs/1";
         const result = {
             "id": 1,
-            "title": "Rave On!",
-            "artist": "Buddy Holly",
-            "url": "https://www.youtube.com/watch?v=0IUV-QxwlRM"
+            "title": "La Mer (Beyond the Sea)",
+            "artist": "Bobby Darin",
+            "url": "https://www.youtube.com/watch?v=m8OlDPqYBLw"
         };
     
         let response = await request(app).get(url);
